343c899a28d046d333d33fac9dae9982bd90f944
Vanessa Rivera <10712121+rivessa@users.noreply.github.com>
2025-01-15 14:18:13 -0800

    Add static method examples

--- src/Module2/CatComplete.java
+++ src/Module2/CatComplete.java
@@ -28,10 +28,22 @@ public class CatComplete {
     }
 
     public void getPets(int number) {
-        System.out.printf("%s accepts %d pets.", name, number);
+        System.out.printf("%s accepts %d pets.", this.name, number);
     }
 
     public static void getPets(CatComplete cat, int number) {
-        System.out.printf("%s accepts pets.", cat.name, number);
+        System.out.printf("%s accepts %d pets.", cat.name, number);
+    }
+
+    public boolean isNameGood() {
+        return (this.name == null ? false : !this.name.toLowerCase().equals("pearl"));
+    }
+
+    public static boolean isNameGood(CatComplete c) {
+        return (c.name == null ? false : !c.name.toLowerCase().equals("pearl"));
+    }
+
+    public static boolean isNameGood(String name) {
+        return (name == null ? false : !name.toLowerCase().equals("pearl"));
     }
 }
